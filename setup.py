5e6f0b123cc4f711333ebd0477e53be318501ab5
Ross Delinger <rossdylan@csh.rit.edu>
2012-07-03 12:40:41 -0400

    Initial code in setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.joins(here, 'README.rst')).read()
+
+requires = [
+        'tahrir-api',
+        'flask'
+        ]
+
+setup(
+    version='0.1.0',
+    description='A webapp which exposes a restful api for tahrir',
+    long_description=readme,
+    license='AGPLv3+',
+    classifiers=[
+        "programming Language :: Python",
+        "Framework :: Flask",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+    ],
+    author="Ross Delinger",
+    author_email="rossdylan@csh.rit.edu",
+    url="https://github.com/rossdylan/tahrir-rest",
+    packages = ["tahrir_rest",],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires)
