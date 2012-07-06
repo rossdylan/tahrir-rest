f3f06f7ae3de2885016923b24f87f0f317b92dc3
Ross Delinger <rossdylan@csh.rit.edu>
2012-07-06 12:58:19 -0400

    Added method stubs for interacting with badges

--- tahrir_rest/__init__.py
+++ tahrir_rest/__init__.py
@@ -9,6 +9,25 @@ class TahrirRestApp(object):
         self.database = TahrirDatabase(self.dburi)
         self.app = Flask(__name__)
 
+    def get_badge(self, uid):
+        """
+        GET: /badges/<uid>
+        return s a JSON blob with all the badge attributes
+        """
+        pass
+
+    def add_badge(self):
+        """
+        POST: /badges
+        accepts a json blob with all the badge attributes
+        """
+        pass
+
+    def remove_badge(self):
+        """
+        DELETE /badges/<uid>
+        Deletes a badge with the given uid
+        """
 
 
 def main(globalArgs, **localArgs):
